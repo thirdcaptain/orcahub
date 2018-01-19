9230f4c68c2d0bc24bea5cb83b887ad46c3b94e0
Ubuntu <ubuntu@ip-10-250-1-226.ec2.internal>
2018-01-19 23:30:37 +0000

    updates to endpoint

--- webapp.py
+++ webapp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Flask app to generate complete html page containing location/amenity
 dropdown menus and rental listings"""
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, abort
 from flask_cors import CORS
 
 from os import getenv
@@ -15,18 +15,23 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 def authenticate_user():
     code = request.args.get('code')
     if code:
+        print(code)
         data = {'code': code}
         data['client_id'] = getenv('CLIENT_ID')
         data['client_secret'] = getenv('CLIENT_SECRET')
         r = requests.post('https://github.com/login/oauth/access_token',
-                          data=data, headers={'Accept': 'application/json'})
-        if r.status_code == 200:
+                          params=data, headers={'Accept': 'application/json'})
+        try:
+            r.raise_for_status()
             auth_json = r.json()
             redirect_to_repos = redirect('https://orcas.holberton.us/repos')
             response = make_response(redirect_to_repos)
             response.set_cookie('access_token',
                                 value=auth_json.get('access_token'))
             return response
+        except:
+            print(r.status_code)
+            return abort(404)
     else:
         return redirect('https://orcas.holberton.us')
 
